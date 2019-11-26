0d05837871434467964322ea422b43c240f170f4
borko_home <bstoilov81@gmai.com>
2019-11-26 20:25:10 +0200

    Inlcude shablon's

--- checksistem/urls.py
+++ checksistem/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from checksistem import views
 
 urlpatterns = [
-    path('', views.index, name="all_include"),
+	# URL's
+	# Начална страница
+    path('', views.index, name="begin"),
+    # Конфигурация на карти
+    path('cards/', views.cards, name="cards"),
+    # Конфигурация на групи
+    path('groups/', views.groups, name="groups"),
+    # Конфигурация на служители
+    path('employe/', views.groups, name="groups"),
 ]
--- checksistem/views.py
+++ checksistem/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+import os
+from collections import defaultdict
 
 
 def index(request):
-    return HttpResponse("Test")
+    return render(request, "checksistem/index.html", {})
+
+
+def cards(request):
+    return render(request, "checksistem/cards.html", {})
+
+
+def groups(request):
+    return render(request, "checksistem/groups.html", {})
+
+
+def employe(request):
+    return render(request, "checksistem/employe.html", {})
